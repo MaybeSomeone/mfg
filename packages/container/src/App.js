777dd712403551a5d77c5538dfe7b2baa07260b3
MaybeSomeone <329237486@qq.com>
2024-07-10 15:04:52 +0800

    update the app.js code

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div>
-            <h1>Marketing App</h1>
+            <h1>Marketing App need to test</h1>
             <MarketingApp />
         </div>
     )
